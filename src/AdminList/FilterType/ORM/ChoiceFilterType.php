e302e7de842e1873b4fc39a19877e35eecb0ba96
Gabe <hgabka@dev.hgabka.eu>
2017-09-20 18:10:05 +0200

    choice filter

---
+++ src/AdminList/FilterType/ORM/ChoiceFilterType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Hgabka\KunstmaanExtensionBundle\AdminList\FilterType\ORM;
+
+use Kunstmaan\AdminListBundle\AdminList\FilterType\ORM\AbstractORMFilterType;
+use Symfony\Component\HttpFoundation\Request;
+
+class ChoiceFilterType extends AbstractORMFilterType
+{
+    /**
+     * @param Request $request  The request
+     * @param array   &$data    The data
+     * @param string  $uniqueId The unique identifier
+     */
+    public function bindRequest(Request $request, array &$data, $uniqueId)
+    {
+        $data['comparator'] = $request->query->get('filter_comparator_'.$uniqueId);
+        $data['value'] = $request->query->get('filter_value_'.$uniqueId);
+    }
+
+    /**
+     * @param array  $data     The data
+     * @param string $uniqueId The unique identifier
+     */
+    public function apply(array $data, $uniqueId)
+    {
+        if (isset($data['value']) && isset($data['comparator'])) {
+            $colName = stripos($this->columnName,'.') === false ? $this->getAlias().$this->columnName : $this->columnName;
+
+            switch ($data['comparator']) {
+                case 'equals':
+                    $this->queryBuilder->andWhere($this->queryBuilder->expr()->eq($colName, ':var_'.$uniqueId));
+                    $this->queryBuilder->setParameter('var_'.$uniqueId, $data['value']);
+
+                    break;
+                case 'notequals':
+                    $this->queryBuilder->andWhere($this->queryBuilder->expr()->neq($colName, ':var_'.$uniqueId));
+                    $this->queryBuilder->setParameter('var_'.$uniqueId, $data['value']);
+
+                    break;
+            }
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getTemplate()
+    {
+        return 'HgabkaKunstmaanExtensionBundle:FilterType:ChoiceFilter.html.twig';
+    }
+}
