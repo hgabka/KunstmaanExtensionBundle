96eb332efc559f08d40d7b5349f2c8e8c7ea472b
hgabka <hgabka@gmail.com>
2019-05-24 12:27:20 +0200

    Update HgabkaKunstmaanExtensionBundle.php

--- src/HgabkaKunstmaanExtensionBundle.php
+++ src/HgabkaKunstmaanExtensionBundle.php
@@ -2,8 +2,20 @@
 
 namespace Hgabka\KunstmaanExtensionBundle;
 
+use Doctrine\DBAL\Types\Type;
+use Hgabka\KunstmaanExtensionBundle\Doctrine\Type\LongblobType;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class HgabkaKunstmaanExtensionBundle extends Bundle
 {
+    public function boot()
+    {
+        foreach ($this->container->getParameter('doctrine.entity_managers') as $name => $serviceName) {
+            $em = $this->container->get($serviceName);
+            if (!Type::hasType(LongblobType::TYPE)) {
+                Type::addType(LongblobType::TYPE, LongblobType::class);
+                $em->getConnection()->getDatabasePlatform()->registerDoctrineTypeMapping('longblob', LongblobType::TYPE);
+            }
+        }
+    }
 }
